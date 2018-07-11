c0e2ef370e084dcc089d4d421b868e8870180a1b
CatOrange <wangrusheng12@163.com>
2018-07-11 18:52:13 +0800

    format string

---
+++ exe04.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8-*-
+
+name = "Mkile"
+age = 28
+weight = 70
+height = 180
+
+#print(f"Let's talk about {name}.")
+print(f"Let's talk about {name}.")
+print(f"He's :{height} inches tall")
+total = age + weight + height
+print(f"If I add{age},{weight},{height}I get {total}.")
+
+
