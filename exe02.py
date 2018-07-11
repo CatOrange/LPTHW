e02aa011aeded6d9c58e4862feec81f6119e1291
CatOrange <wangrusheng12@163.com>
2018-07-11 17:20:44 +0800

    numbers and math

---
+++ exe02.py
@@ -0,0 +1,17 @@
+#-*-coding:utf-8-*-
+
+print("I will now count my chickens:")
+
+print("Hens:",28+30/6)
+print("Roosters:", 100-28*3%4)
+
+print("now I will count the eggs:")
+print(3+2-4+5+4%2-1)
+
+print("is that true 3<4")
+print( 3<4)
+
+print(3-4)
+
+print("is it greater?", 5>2)
+
